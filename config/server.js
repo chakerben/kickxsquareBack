ef6b851fc6fc337704889239131db6e0f3aaa6fd
Ben Moussa Chaker <chaker.info@gmail.com>
2021-11-29 22:44:39 +0100

    add autoreload

--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'aa3846a3a1240ba9fbfef763fa54af2c'),
+      secret: env("ADMIN_JWT_SECRET", "aa3846a3a1240ba9fbfef763fa54af2c"),
     },
+  },
+  autoReload: {
+    enabled: true,
   },
 });
